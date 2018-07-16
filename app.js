7fb7975c1fec3775550400e5e896f9007b72e5e8
David Shuster <shusterdavidminsk@gmail.com>
2018-07-16 08:53:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import { express } from 'express';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs, resolvers } from './schema';
+
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
